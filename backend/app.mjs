c7c9644cdce658fa29d805520c7847dc0f580ea5
2161351 <2161351@my.ipleiria.pt>
2023-02-18 19:34:37 +0000

    Update country method

--- backend/app.mjs
+++ backend/app.mjs
@@ -1,6 +1,7 @@
 import db from "./db/conn.mjs";
 
 import express from "express";
+import {ObjectId} from "mongodb";
 
 const app = express()
 const port = 3000
@@ -32,6 +33,47 @@ app.post('/countries', async (req, res) => {
     
     if(result.acknowledged == true){
       let successMsg = { error: false, msg:"Country successfully created."} 
+      res.send(successMsg).status(200);
+    }else{
+      let errorMsg = { error: true, msg: result}
+      res.send(errorMsg).status(500);
+    }
+  }
+});
+
+app.put('/countries/:id', async (req, res) => {
+  let collection = await db.collection("countries");  
+    
+  if(req.params.id=="" || req.params.id ==undefined){
+    let errorMsg ={ error: true, msg: "Record not found."};
+    res.send(errorMsg).status(404);
+    return ;
+  }
+
+  let query = {_id: new ObjectId(req.params.id.toString())};
+
+  let country = await collection.findOne(query);
+
+  if(country ==undefined || country == null){
+    let errorMsg ={ error: true, msg: "Record not found."};
+    res.send(errorMsg).status(404);
+    return ;
+  }
+
+  let body = req.body;
+
+  if(body.name == "" || body.name==undefined){
+    let errorMsg ={ error: true, msg: "The field name is required."};
+    res.send(errorMsg).status(400);
+  }else{
+    const updates = {
+      $push: { countries: body }
+    };
+
+    let result = await collection.updateOne(query, updates);
+
+    if(result.acknowledged == true){
+      let successMsg = { error: false, msg:"Country successfully updated."} 
       res.send(successMsg).status(200);
     }else{
       let errorMsg = { error: true, msg: result}
