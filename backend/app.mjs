7b509e82cd1083b5d108d4a1c290af42fb175032
2161351 <2161351@my.ipleiria.pt>
2023-02-18 19:14:29 +0000

    Create countries method

--- backend/app.mjs
+++ backend/app.mjs
@@ -1,8 +1,12 @@
 import db from "./db/conn.mjs";
 
 import express from "express";
+
 const app = express()
 const port = 3000
+
+app.use(express.json());
+
 
 app.get('/countries', async (req, res) => {
   let collection = await db.collection("countries");
@@ -11,8 +15,32 @@ app.get('/countries', async (req, res) => {
     .toArray();
 
   res.send(results).status(200);
-})
+});
+
+app.post('/countries', async (req, res) => {
+  let collection = await db.collection("countries");  
+  
+  let body = req.body;
+
+  if(body.name == "" || body.name==undefined){
+    let errorMsg ={ error: true, msg: "The field name is required."};
+    res.send(errorMsg).status(400);
+  }else{
+    let newCountry = body;
+
+    let result = await collection.insertOne(newCountry);
+    
+    if(result.acknowledged == true){
+      let successMsg = { error: false, msg:"Country successfully created."} 
+      res.send(successMsg).status(200);
+    }else{
+      let errorMsg = { error: true, msg: result}
+      res.send(errorMsg).status(500);
+    }
+  }
+});
+
 
 app.listen(port, () => {
   console.log(`Birthday backend API app listening on port ${port}`)
-})
+});
