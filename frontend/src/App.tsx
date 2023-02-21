aa0f7b285617ac3f1e1a281ffd6dfa7be350f04e
2161351 <2161351@my.ipleiria.pt>
2023-02-21 20:21:14 +0000

    Add loading gif when fetching/saving info

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,6 +9,7 @@ import Alert from 'react-bootstrap/Alert';
 import { useEffect, useState } from 'react';
 import axios from "axios";
 import moment from 'moment';
+import { Spinner } from 'react-bootstrap';
 
 
 interface Country {
@@ -28,29 +29,50 @@ export default function App() {
 
   const [countries, setCountries] = useState(Array<Country>);
   const [persons, setPersons] = useState(Array<Person>);
-  const [isSaved, setSaved] = useState<boolean>(false);
+  const [isLoading, setLoading] = useState<boolean>(true);
+  const [isSaving, setSaving] = useState<boolean>(false);
 
-  const [currentPerson, setCurrentPerson] = useState<Person>({ name: '', surname: '', country: '', birthday: moment().format('DD/MM/YYYY'), years:0});
+  const [alertPerson, setAlertPerson] = useState<Person>({ name: '', surname: '', country: '', birthday: moment().format('DD/MM/YYYY'), years: 0 });
+  const [newPerson, setNewPerson] = useState<Person>({ name: '', surname: '', country: '', birthday: moment().format('DD/MM/YYYY'), years: 0 });
+
 
   useEffect(() => {
-    getCountries();
-  }, [])
+    if (countries.length === 0) {
+      getCountries();
+    }
+  }, [countries])
 
   const getCountries = () => {
     axios.get('http://localhost:3001/countries').then((response) => {
       setCountries(response.data);
+      setLoading(false);
     })
   }
 
   const saveNewPerson = (e: any) => {
     e.preventDefault();
+    setPersons([...persons, newPerson]);
     
-    var years = moment().diff(currentPerson.birthday, 'years',true);
+    setSaving(true);
+    
+    setTimeout(() => {
+      setSaving(false);
+      setNewPerson({ name: '', surname: '', country: '', birthday: moment().format('DD/MM/YYYY'), years: 0 })
+    }, 500);
 
-    setCurrentPerson({...currentPerson , years: Math.floor(years)});
+    setAlert(newPerson);
+  }
 
-    setPersons([...persons, currentPerson]);
-    setSaved(true);
+  const setAlert = (person: Person) => {
+    var years = moment().diff(person.birthday, 'years', true);
+
+    person.years= Math.floor(years);
+
+    setAlertPerson(person);
+    
+    setTimeout(() => {
+      setAlertPerson({ name: '', surname: '', country: '', birthday: moment().format('DD/MM/YYYY'), years: 0 });
+    }, 4000);
   }
 
   return (
@@ -61,76 +83,105 @@ export default function App() {
         </Col>
       </Row>
       <Row>
-        <Col>
-          <Form>
-            <Form.Group as={Row} className="mb-3" controlId="formName">
-              <Form.Label column sm={2}>Name:</Form.Label>
-              <Col sm={10}>
-                <Form.Control value={currentPerson.name} onChange={(e) => setCurrentPerson({ ...currentPerson, name: e.target.value })} type="text" placeholder="Name here" />
-              </Col>
-            </Form.Group>
-            <Form.Group as={Row} className="mb-3" controlId="formSurname">
-              <Form.Label column sm={2}>Surname:</Form.Label>
-              <Col sm={10}>
-                <Form.Control value={currentPerson.surname} onChange={(e) => setCurrentPerson({ ...currentPerson, surname: e.target.value })} type="text" placeholder="Surname here" />
-              </Col>
-            </Form.Group>
-            <Form.Group as={Row} className="mb-3" controlId="formCountry">
-              <Form.Label column sm={2}>Country:</Form.Label>
-              <Col sm={10}>
-                <Form.Select defaultValue = {currentPerson.country} aria-label="Default select example" onChange={(e) => setCurrentPerson({ ...currentPerson, country: e.target.value })}>
-                  <option>Countries</option>
-                  {countries.map((item: Country) => {
-                    return (
-                      <option key={item._id} value={item.name}>{item.name}</option>
-                    )
-                  })}
-                </Form.Select>
-              </Col>
-            </Form.Group>
+        {isLoading ?
+          <Col className="text-center">
+            <Spinner animation="border" />
+          </Col>
+          :
+          <>
+            <Col>
+              <Form>
+                <Form.Group as={Row} className="mb-3" controlId="formName">
+                  <Form.Label column sm={2}>Name:</Form.Label>
+                  <Col sm={10}>
+                    <Form.Control value={newPerson.name} onChange={(e) => setNewPerson({ ...newPerson, name: e.target.value })} type="text" placeholder="Name here" />
+                  </Col>
+                </Form.Group>
+                <Form.Group as={Row} className="mb-3" controlId="formSurname">
+                  <Form.Label column sm={2}>Surname:</Form.Label>
+                  <Col sm={10}>
+                    <Form.Control value={newPerson.surname} onChange={(e) => setNewPerson({ ...newPerson, surname: e.target.value })} type="text" placeholder="Surname here" />
+                  </Col>
+                </Form.Group>
+                <Form.Group as={Row} className="mb-3" controlId="formCountry">
+                  <Form.Label column sm={2}>Country:</Form.Label>
+                  <Col sm={10}>
+                    <Form.Select value={newPerson.country} aria-label="Default select example" onChange={(e) => setNewPerson({ ...newPerson, country: e.target.value })}>
+                      <option>Countries</option>
+                      {countries.map((item: Country) => {
+                        return (
+                          <option key={item._id} value={item.name}>{item.name}</option>
+                        )
+                      })}
+                    </Form.Select>
+                  </Col>
+                </Form.Group>
 
-            <Form.Group as={Row} className="mb-3" controlId="formBirthday">
-              <Form.Label column sm={2}>Birthday:</Form.Label>
-              <Col sm={10}>
-                <Form.Control type="date" value = {currentPerson.birthday} onChange={(e) =>setCurrentPerson({ ...currentPerson, birthday: moment(new Date(e.target.value)).format('YYYY-MM-DD')})} />
-              </Col>
-            </Form.Group>
-            <Row>
-              <Col style={{ textAlign: 'right' }} md={{ span: 2, offset: 10 }}>
-                <Button onClick={(e)=>saveNewPerson(e)}  variant="primary" type="submit" >
-                  Save
-                </Button>
-              </Col>
-            </Row>
-          </Form>
-          {isSaved && 
-            <Alert variant="success" className="mt-2">
-              Hello {currentPerson.name} from {currentPerson.country} on {moment(currentPerson.birthday).format('D')} of {moment(currentPerson.birthday).format('MMMM')} you will be {currentPerson.years} old!
-            </Alert>
-          }
-        </Col>
-        <Col>
-          <Table bordered>
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>Country</th>
-                <th>Birthday</th>
-              </tr>
-            </thead>
-            <tbody>
-              {persons.map((person: Person, idx: number) => {
-                return (
-                  <tr key={idx} onClick={()=>setCurrentPerson(person)}>
-                    <td>{person.name} {person.surname}</td>
-                    <td>{person.country}</td>
-                    <td>{person.birthday.toString()}</td>
+                <Form.Group as={Row} className="mb-3" controlId="formBirthday">
+                  <Form.Label column sm={2}>Birthday:</Form.Label>
+                  <Col sm={10}>
+                    <Form.Control type="date" value={newPerson.birthday} onChange={(e) => setNewPerson({ ...newPerson, birthday: moment(new Date(e.target.value)).format('YYYY-MM-DD') })} />
+                  </Col>
+                </Form.Group>
+                <Row>
+                  <Col style={{ textAlign: 'right' }} md={{ span: 2, offset: 10 }}>
+                    {!isSaving ?
+                      <Button onClick={(e) => saveNewPerson(e)} variant="primary" type="submit" >
+                        Save
+                      </Button>
+                      :
+                      <Button variant="primary" disabled>
+                        <Spinner
+                          as="span"
+                          animation="grow"
+                          size="sm"
+                          role="status"
+                          aria-hidden="true"
+                        />
+                        Saving...
+                      </Button>
+                    }
+                  </Col>
+                </Row>
+              </Form>
+              {!isSaving && alertPerson.name!=='' &&
+                <Alert variant="success" className="mt-2">
+                  Hello {alertPerson.name} from {alertPerson.country} on {moment(alertPerson.birthday).format('D')} of {moment(alertPerson.birthday).format('MMMM')} you will be {alertPerson.years} old!
+                </Alert>
+              }
+            </Col>
+            <Col>
+              <Table bordered>
+                <thead>
+                  <tr>
+                    <th>Name</th>
+                    <th>Country</th>
+                    <th>Birthday</th>
                   </tr>
-                )
-              })}
-            </tbody>
-          </Table>
-        </Col>
+                </thead>
+                <tbody>
+                  {isSaving ?
+                    <tr>
+                      <td colSpan={3} className="text-center">
+                        <Spinner animation="border" />
+                      </td>
+                    </tr>
+                    :
+                    persons.map((person: Person, idx: number) => {
+                      return (
+                        <tr key={idx} onClick={() => setAlert(person)}>
+                          <td>{person.name} {person.surname}</td>
+                          <td>{person.country}</td>
+                          <td>{person.birthday.toString()}</td>
+                        </tr>
+                      )
+                    })
+                  }
+                </tbody>
+              </Table>
+            </Col>
+          </>
+        }
       </Row>
     </Container>
   );
